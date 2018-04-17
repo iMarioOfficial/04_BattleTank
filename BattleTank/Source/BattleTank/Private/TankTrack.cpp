e690e4715186b0bb8f7139280d9180f27b3b2218
Mario Pardo <mario.pardo003@gmail.com>
2018-04-16 23:32:32 -0400

    Added Simple Driving

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Throttle Value is : %f"), *Name, Throttle)
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent()); // cast so we can add forces to it
+
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
+
